dd7c2ae5beb252bb5236a5d5cbf88a1f7dcdeebb
Giianmachado <giianmachado@live.com>
2019-02-23 22:14:25 -0300

    Find customer by id

--- src/main/java/com/example/easynotes/controller/CustomerController.java
+++ src/main/java/com/example/easynotes/controller/CustomerController.java
@@ -5,11 +5,13 @@ import com.example.easynotes.repository.CustomerRepository;
 import com.example.easynotes.repository.PhoneRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +31,11 @@ public class CustomerController {
     @PostMapping("/customers")
     public Customer createCustomer(@Valid @RequestBody Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    @GetMapping("/customers/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable(value = "id") Long customer_id) {
+        Optional<Customer> customer = customerRepository.findById(customer_id);
+        return ResponseEntity.ok().body(customer.get());
     }
 }
